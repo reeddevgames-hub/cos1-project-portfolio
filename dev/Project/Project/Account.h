3d9925b6eec25a11fb6a1b5b86686b76abb8348e
Matthew Reed <MFReed1@student.fullsail.edu>
2026-08-07 23:48:35 -0700

    Code written for Account class (Untested).

--- dev/Project/Project/Account.h
+++ dev/Project/Project/Account.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<cstring>
 
 class Account
 
@@ -7,10 +8,48 @@ class Account
 {
 private:
 	
-	int account_Number;
-	char name[50];
-	double balance;
+	int account_Number;               // Account Number, unique Identifier.
+	char holder_Name[50];            // Account Holders name in a char array with a max of 50 characters for ease of use with binary writing to file.
+	double total_Balance;              // The Account balances tied to that Account Holder. 
 
+public:
+
+	// Constructor
+	Account( int accId, const char* name, double balance) : account_Number(accId), total_Balance(balance) 
+	{
+		strcpy_s(holder_Name, 50, name);
+	}
+
+	// Getters
+	int GetAccountNumber() const
+	{
+		return account_Number;
+	}
+
+	const char* GetAccountHolderName() const
+	{
+		return holder_Name;
+	}
+
+	double GetAccountBalance() const
+	{
+		return total_Balance;
+	}
+
+	// Setters
+	void SetAccountNumber(int acc_Number)
+	{
+		account_Number = acc_Number;
+	}
+
+	void SetAccountHolderName(const char* _name)
+	{
+		strcpy_s(holder_Name, 50, _name);
+	}
+
+	bool Deposit();
+
+	bool Withdraw();
 
 };
 
