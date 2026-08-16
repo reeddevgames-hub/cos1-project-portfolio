d2edf62743fa2da2a4448831f47fbd213f3d6038
testUsername <MFReed1@student.fullsail.edu>
2026-08-15 19:19:17 -0700

    Created InputFilter functions.

--- dev/Project/Project/InputFilter.h
+++ dev/Project/Project/InputFilter.h
@@ -1,9 +1,18 @@
 #pragma once
+#include<string>
 
 // This class will be used to make sure that the user inputs the right symboles, letters or, numbers,
 // so the console application does not crash.
 class InputFilter
 {
+public:
+	// Will be used to filter out any invalid inputs (like letters) and loop till a proper integer is accepted. 
+	static int SafeInteger(const char* promptMessage, const char* errorMessage);
 
+	// Will be used to filter out any invalid inputs for doubles such as letters and numbers less than zero, and loop till a positive double is accepted. 
+	static double SafePositiveDouble(const char* promptMessage, const char* errorMessage);
+
+	// Will be used to filter out any invalid inputs and loop till a string input is accepted. 
+	static std::string SafeString(const char* promptMessage, const char* errorMessage);
 };
 
